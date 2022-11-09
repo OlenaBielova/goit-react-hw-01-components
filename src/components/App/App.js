b7e67736bec65d7a869d2e01ba825edabffba4ce
OlenaBielova <belova.elena11@gmail.com>
2022-11-09 12:45:35 +0200

    add statistics and transactions history sections

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,10 @@ import Profile from 'components/Profile/Profile';
 import user from 'user.json';
 import Statistics from 'components/Statistics/Statistics';
 import data from 'data.json';
+import FriendList from 'components/FriendList/FriendList';
+import friends from 'friends.json';
+import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
+import transactions from 'transactions.json';
 
 function App() {
   return (
@@ -15,9 +19,11 @@ function App() {
         location={user.location}
         avatar={user.avatar}
         stats={user.stats} />
-      <Statistics title="Upload stats" stats={data}/>
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,3 +1,25 @@
-import css from './FriendList.css';
+import css from './FriendList.module.css';
 import PropTypes from 'prop-types';
 
+const FriendList = ({ friends }) => {
+    return <ul className={css.friend_list}>
+    {friends.map(friend => 
+        <li className={css.item} key={friend.id}>
+            {friend.isOnline?<span className={css.status_online}></span>:<span className={css.status_offline}></span>}
+            <img className={css.avatar} src={friend.avatar} alt="User avatar" width="48" />
+            <p className={css.name}>{friend.name}</p>
+    </li>)
+    }
+    </ul>
+}
+
+FriendList.propTypes = {
+    friends: PropTypes.arrayOf(PropTypes.exact({
+        avatar: PropTypes.string,
+        name: PropTypes.string.isRequired,
+        isOnline: PropTypes.bool.isRequired,
+        id: PropTypes.number.isRequired
+    }))
+}
+
+export default FriendList;
